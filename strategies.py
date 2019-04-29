4ec382d76118076705f4c809048106aa39a6d07d
ryumaggs <ryu@colgate.edu>
2019-04-29 10:11:28 -0400

    added DBS and poke

--- strategies.py
+++ strategies.py
@@ -217,50 +217,346 @@ class DBS():
 	def __init__(self):
 		self.title = "DBS"
 		self.move_history = []
-		self.outcome_history = []
-		self.opponent_schema = {(0,0): 1,(0,1): 1,(1,0): 0,(1,1): 0}
+		self.outcome_history = {}
+		self.opponent_schema = {(0,0): 1,(0,1): 1,(1,0): 0,(1,1): 0} #probabilistic
+		self.opponent_strategies = {} #known
 		self.cur_opponent_name = ""
 		self.test_phase_counter = 0
 
 	def decision(self):
-		if opponent_name != cur_opponent_name:
+		#not sure if i need this., think we make a new player each round
+		if 1 == 0:
 			self.move_history = []
-			self.outcome_history = []
-			self.opponent_schema = {(0,0): 1,(0,1): 1,(1,0): 0,(1,1): 0}
+			self.outcome_history = {} #takes previous moves as key, and current move as value
+			self.opponent_schema = {(0,0): 1,(0,1): 1,(1,0): 0,(1,1): 0} #these are probabilistic
+			self.opponent_strategies = {} #these are known strategies
 			self.test_phase_counter = 0
 			return 0
 		else:
-			if self.test_phase_counter < 20:
-				self.test_phase_counter += 1
-				if len(self.move_history) == 0:
+			if len(self.move_history) == 0:
+				return 0
+			last_move = self.move_history[-1]
+			if last_move in self.opponent_strategies:
+				if self.opponent_strategies[last_move] == 0:
 					return 0
 				else:
-					best = 0
-					strat = 0
-					for keys in opponent_schema:
-						if opponent_schema[keys] >= best:
-							best = opponent_schema[keys]
-							strat = keys[1]
-					return strat
+					return 1
 			else:
-				self.test_phase_counter += 1
-				best = 0
-				strat = 0
-				for keys in opponent_schema:
-					if opponent_schema[keys] >= best:
-						best = opponent_schema[keys]
-						strat = keys[1]
-				return strat
-	def record(self,opp_move,my_move):
-		self.move_history.append((opp_move,my_move))
+				collude_probability = self.opponent_schema[last_move]
+				if collude_probability > .6:
+					return 0
+				else:
+					return 1
 
-	def update_schema(self,move_history,opponent_schema):
+	def record(self,opp_move,my_move):
+		#add test phase recording here l0l
+		#FIRST. TO TAKE CARE OF 100% STRATEGIES:
+		#takes care of the first round
+		if len(self.move_history) == 0:
+			self.move_history.append((my_move,opp_move))
+			return
+		#current timeline
+		self.move_history.append((my_move,opp_move))
+
+		l = list(self.move_history[-1])
+		print("aa",l)
+		l_t = tuple(l)
+		l.append(opp_move)
+		print("bb",l)
+		t = tuple(l)
+
+		#alternate timeline
+		alt = 0
+		alt_ochist = []
+		if opp_move == 1:
+			l.append(0)
+		else:
+			alt = 1
+			l.append(1)
+		
+		alt_ochist = tuple(l)
+
+		#records previous round with new opponent choice. If it is an unseen option, make new dictionary entry
+		#records (prev_my_move,prev_opp_move,current_opp_move)
+		if t in self.outcome_history:
+			if self.outcome_history[t] < 3:
+				self.outcome_history[t] += 1 #records previous opponent move with current choice
+			#an opponent has repeated the same strategy 3 times, record it as a permanent strategy
+			if self.outcome_history[t] == 3 and l_t not in self.opponent_strategies:
+				self.opponent_strategies[l_t] = opp_move
+		else:
+			self.outcome_history[t] = 0
+
+		#if i recorded an opponent strategy, but they did the opposite
+		#then decrement their promotion count by 1
+		if l_t in self.opponent_strategies and self.opponent_strategies[l_t] == alt:
+			self.outcome_history[alt_ochist] -= 1
+			#if a permanent strategy fails 3 times in a row, delete it from learned strategies
+			if self.outcome_history[alt_ochist] == 0:
+				del self.opponent_strategies[l_t]
+
+		if l_t in self.opponent_strategies:
+			opponent_choice = self.opponent_strategies[l_t]
+			if opponent_choice == 1:
+				return 1
+			else:
+				return 0
+		#only updated schema if pure strategy cannot be found
+		self.update_schema()
+
+		likely_opponent_choice = self.opponent_schema[l_t]
+		if likely_opponent_choice > .6:
+			return 0
+		else:
+			return 1
+		#OK NOW WE HAVE TO TAKE CARE OF PROBABILISTIC ONES
+
+	def update_schema(self):
+		print("updating schema")
 		alpha = 0.75
-		for keys in opponent_schema:
-			p_k_1_num = 0
-			p_k_1_denom = 0
-			i = 1
-			while(i < len(move_history)):
-				if move_history[i-1][1] == 'C':
-					proportion = i/len(move_history)
-					temp_num = proportion*alpha
+		lead_to_collude = 0
+		lead_to_betray = 0
+		i = 0
+		for i in range(len(self.move_history)-1):
+			if self.move_history[i] == self.move_history[-1]:
+				if self.move_history[i+1][1] == 0:
+					lead_to_collude += 1 * (i/len(self.move_history))
+				else:
+					print("i got betrayed")
+					lead_to_betray += 1 * (i/len(self.move_history))
+		total = lead_to_collude + lead_to_betray
+		if total > 0:
+			self.opponent_schema[self.move_history[-1]] = lead_to_collude/total
+
+	def printall(self):
+		print("")
+		print("SCHEMA: ", self.opponent_schema)
+		print("KNOWN STRAT: ", self.opponent_strategies)
+		print("outcome_history: ", self.outcome_history)
+		print("move_history: ", self.move_history)
+		print("")
+
+
+class simplePrediction():
+	def __init__(self):
+		self.opponent_dict = {}
+
+	def decision(self):
+		#if options are tied, will assume that they will collude (0)
+		if len(self.opponent_dict) == 0:
+			return 0
+		Keymax = max(self.opponent_dict, key=self.opponent_dict.get)
+		if Keymax == 0:
+			return 0
+		else:
+			return 1
+
+	def record(self,opp_move,my_move):
+		if opp_move in self.opponent_dict:
+			self.opponent_dict[opp_move] += 1
+		else:
+			self.opponent_dict[opp_move] = 0
+
+class poke():
+	def __init__(self):
+		self.title = "DBS"
+		self.move_history = []
+		self.outcome_history = {}
+		self.opponent_schema = {(0,0): 1,(0,1): 1,(1,0): 0,(1,1): 0} #probabilistic
+		self.opponent_strategies = {} #known
+		self.cur_opponent_name = ""
+		self.test_phase_counter = 0
+		self.collude_tracker = 0
+		self.collude_limit = 3
+		self.betray_test = False
+		self.betray_tracker = 0 #could have gotten 3 points if continued to collude
+		self.potential_points = 0
+		self.betray_probability = 1
+		self.test_limit = 3
+
+	def decision(self):
+		#not sure if i need this., think we make a new player each round
+		if 1 == 0:
+			self.move_history = []
+			self.outcome_history = {} #takes previous moves as key, and current move as value
+			self.opponent_schema = {(0,0): 1,(0,1): 1,(1,0): 0,(1,1): 0} #these are probabilistic
+			self.opponent_strategies = {} #these are known strategies
+			self.test_phase_counter = 0
+			return 0
+		else:
+			if len(self.move_history) == 0:
+				return 0
+			last_move = self.move_history[-1]
+			if last_move in self.opponent_strategies:
+				if self.opponent_strategies[last_move] == 0:
+					#if I am not already in the middle of betray testing and my conditions have been met
+					if self.betray_test == False and self.collude_tracker == self.collude_limit:
+						b = random.randint(0,100)
+						if b <= betray_probability * 100:
+							self.collude_tracker = 0
+							self.betray_test = True
+							self.betray_tracker = 0
+							return 1
+					else:
+						return 0
+				else:
+					return 1
+			else:
+				collude_probability = self.opponent_schema[last_move]
+				if collude_probability > .6:
+					if self.collude_tracker == 3:
+						self.betray_test = True
+						self.betray_tracker = 0
+						return 1
+					return 0
+				else:
+					return 1
+
+	def record(self,opp_move,my_move):
+		#add test phase recording here l0l
+		#FIRST. TO TAKE CARE OF 100% STRATEGIES:
+		#takes care of the first round
+
+		if self.betray_test == True:
+			self.betray_tracker += 1
+			if opp_move == 0 and my_move == 0:
+				self.potential_points + 1
+			elif opp_move == 0 and my_move == 1:
+				self.potential_points + 2
+			if self.betray_tracker == self.test_limit:
+				self.collude_tracker = 0
+				self.betray_test = False
+				self.betray_tracker = 0
+				#if i did well. make it more likely to betray more often
+				if self.potential_points > 3:
+					if self.betray_probability < 1:
+						self.betray_probability += .1
+					if self.collude_limit > 0:
+						self.collude_limit = self.collude_limit - 1
+					if self.test_limit > 0:
+						self.test_limit -= 1
+				else:
+				#if test went badd, make it less likely and less often to betray
+					if self.betray_probability > 0:
+						self.betray_probability = self.betray_probability - .1
+					if self.test_limit < 3:
+						self.test_limit += 1
+					if self.collude_limit < 3:
+						self.collude_limit += 1
+				self.potential_points = 0
+
+		if opp_move == 0:
+			self.collude_tracker += 1
+		else:
+			self.collude_tracker = 0
+
+		#if self.collude_tracker >= self.collude_limit:
+	#		self.betray_test = True
+	#		self.betray_tracker = 0
+
+		if len(self.move_history) == 0:
+			self.move_history.append((my_move,opp_move))
+			return
+		#current timeline
+		self.move_history.append((my_move,opp_move))
+
+		l = list(self.move_history[-1])
+		print("aa",l)
+		l_t = tuple(l)
+		l.append(opp_move)
+		print("bb",l)
+		t = tuple(l)
+
+		#alternate timeline
+		alt = 0
+		alt_ochist = []
+		if opp_move == 1:
+			l.append(0)
+		else:
+			alt = 1
+			l.append(1)
+		
+		alt_ochist = tuple(l)
+
+		#records previous round with new opponent choice. If it is an unseen option, make new dictionary entry
+		#records (prev_my_move,prev_opp_move,current_opp_move)
+		if t in self.outcome_history:
+			if self.outcome_history[t] < 3:
+				self.outcome_history[t] += 1 #records previous opponent move with current choice
+			#an opponent has repeated the same strategy 3 times, record it as a permanent strategy
+			if self.outcome_history[t] == 3 and l_t not in self.opponent_strategies:
+				self.opponent_strategies[l_t] = opp_move
+		else:
+			self.outcome_history[t] = 0
+
+		#if i recorded an opponent strategy, but they did the opposite
+		#then decrement their promotion count by 1
+		if l_t in self.opponent_strategies and self.opponent_strategies[l_t] == alt:
+			self.outcome_history[alt_ochist] -= 1
+			#if a permanent strategy fails 3 times in a row, delete it from learned strategies
+			if self.outcome_history[alt_ochist] == 0:
+				del self.opponent_strategies[l_t]
+
+
+		if l_t in self.opponent_strategies:
+			opponent_choice = self.opponent_strategies[l_t]
+			if opponent_choice == 1:
+				return 1
+			else:
+				return 0
+		#only updated schema if pure strategy cannot be found
+		self.update_schema()
+
+		likely_opponent_choice = self.opponent_schema[l_t]
+		if likely_opponent_choice > .6:
+			return 0
+		else:
+			return 1
+		#OK NOW WE HAVE TO TAKE CARE OF PROBABILISTIC ONES
+	def update_schema(self):
+		print("updating schema")
+		alpha = 0.75
+		lead_to_collude = 0
+		lead_to_betray = 0
+		i = 0
+		for i in range(len(self.move_history)-1):
+			if self.move_history[i] == self.move_history[-1]:
+				if self.move_history[i+1][1] == 0:
+					lead_to_collude += 1 * (i/len(self.move_history))
+				else:
+					print("i got betrayed")
+					lead_to_betray += 1 * (i/len(self.move_history))
+		total = lead_to_collude + lead_to_betray
+		if total > 0:
+			self.opponent_schema[self.move_history[-1]] = lead_to_collude/total
+
+	def printall(self):
+		print("")
+		print("SCHEMA: ", self.opponent_schema)
+		print("KNOWN STRAT: ", self.opponent_strategies)
+		print("outcome_history: ", self.outcome_history)
+		print("move_history: ", self.move_history)
+		print("")
+
+
+
+simple = poke()
+dibbs = DBS()
+rounds = 10
+p1_points = 0
+p2_points = 0
+for i in range(rounds):
+	dibbs.printall()
+	simple_choice = simple.decision()
+	dibbs_choice = dibbs.decision()
+	if simple_choice == 0 and dibbs_choice == 0:
+		p1_points += 1
+		p2_points += 1
+	elif simple_choice == 1 and dibbs_choice == 0:
+		p1_points += 2
+	elif dibbs_choice == 1 and simple_choice == 0:
+		p2_points += 2
+
+	print("Current score: ", p1_points, p2_points)
+	simple.record(dibbs_choice,simple_choice)
+	dibbs.record(simple_choice,dibbs_choice)
